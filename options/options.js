1d62b93026bdff8965a06ada11786af0c2c880b9
Crowdbotics <engineering@crowdbotics.com>
2023-04-14 00:17:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "raspy-recipe-dev-76532",
+  url: "https://raspy-recipe-dev-76532.botics.co",
+  api: "https://raspy-recipe-dev-76532.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
